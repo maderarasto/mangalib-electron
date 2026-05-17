d56189c91c18b41f98917148df577ddaef8e79f7
Rastislav Madera <maderarasto@gmail.com>
2026-05-17 14:57:54 +0200

    Added initial check if supabase user has session

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { useAuthStore } from "./store/useAuth";
+import { supabase } from "./lib/supabase";
+import { Spinner } from "./components/shadcn/spinner";
+
 export default function App() {
-  return(
-    <div>
-      App.tsx
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const session = useAuthStore((state) => state.session);
+  const setSession = useAuthStore((state) => state.setSession);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      setSession(data.session);
+      setIsLoading(false);
+    });
+
+    const {data} = supabase.auth.onAuthStateChange((_, session) => {
+      setSession(session);
+    });
+
+    return () => {
+      data.subscription.unsubscribe();
+    }
+  }, []);
+
+  if (isLoading) {
+    return(
+      <div className="flex justify-center items-center w-screen h-screen">
+        <Spinner className="size-10" />
+      </div>
+    );
+  }
+
+  return null;
 }
---
+++ src/store/useAuth.tsx
@@ -0,0 +1,34 @@
+import { AuthUser, Session } from "@supabase/supabase-js";
+import {create} from 'zustand';
+import {devtools} from 'zustand/middleware';
+
+interface AuthState {
+  session: Session|null;
+  user: AuthUser|null;
+
+  setSession: (session: Session|null) => void;
+  reset: () => void;
+};
+
+const initialState: Partial<AuthState> = {
+  session: null,
+  user: null
+};
+
+export const useAuthStore = create<AuthState>()(
+  devtools(
+    (set) => ({
+      ...initialState,
+
+      setSession: (session: Session|null) => set({ 
+        session, 
+        user: session?.user ?? null 
+      }),
+
+      reset: () => set({ 
+        session: null,
+        user: null
+      }),
+    })
+  )
+)
